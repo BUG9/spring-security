979425a8d8e082036c3565d7842d733d595d40fd
zhaohecai <zhciot@163.com>
2019-08-15 18:01:28 +0800

    改用 UserDetailsService 加载用户信息，并解决Security5 版本改动问题 no PasswordEncoder mapped

--- security/src/main/java/com/zhc/security/security/MyUserDetailsUserService.java
+++ security/src/main/java/com/zhc/security/security/MyUserDetailsUserService.java
@@ -17,11 +17,11 @@ public class MyUserDetailsUserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 不能直接使用 创建 BCryptPasswordEncoder 对象来加密
-        // 这种加密方式 没有 {bcrypt}  前缀，
+        // 不能直接使用 创建 BCryptPasswordEncoder 对象来加密， 这种加密方式 没有 {bcrypt}  前缀，
         // 会导致在  matches 时导致获取不到加密的算法出现
         // java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"  问题
-        // 问题原因是 Spring Security5 使用 DelegatingPasswordEncoder  替代 NoOpPasswordEncoder，并且 默认使用  BCryptPasswordEncoder 加密  https://blog.csdn.net/alinyua/article/details/80219500
+        // 问题原因是 Spring Security5 使用 DelegatingPasswordEncoder(委托)  替代 NoOpPasswordEncoder，
+        // 并且 默认使用  BCryptPasswordEncoder 加密（注意 DelegatingPasswordEncoder 委托加密方法BCryptPasswordEncoder  加密前  添加了加密类型的前缀）  https://blog.csdn.net/alinyua/article/details/80219500
         return new User("user",  PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("123456"), AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
